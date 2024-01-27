56606ccf8bac46e36ffc1763e16f52948e173643
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-01-27 14:51:06 -0800

    readme template and header file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* header files */
+#include <unistd.h>
+#include <stdarg.h>
+
+/* function declarations */
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
